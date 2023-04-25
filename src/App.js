f3c4b1309666e5edf1a0504ac6ced27e811df0c0
ankurdev002 <ankursini795@gmail.com>
2023-04-25 10:11:53 +0530

    change in background and product alignment

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import Cart from "./Components/Cart";
+import sample from "./sample.mp4";
 
 const App = () => {
   return (
     <>
+      <video className="videoTag" autoPlay loop muted>
+        <source src={sample} type="video/mp4" />
+      </video>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
